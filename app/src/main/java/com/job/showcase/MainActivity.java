8d905808d05e1c85151c6e849e63776aa03aeb9a
JobGetabu <getabujob@gmail.com>
2018-03-22 11:37:41 +0300

    UI is smooth Added login register activity

--- app/src/main/java/com/job/showcase/MainActivity.java
+++ app/src/main/java/com/job/showcase/MainActivity.java
@@ -1,8 +1,10 @@
 package com.job.showcase;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.job.showcase.adapter.PagerViewAdapter;
@@ -32,5 +34,80 @@ public class MainActivity extends AppCompatActivity {
 
         pagerViewAdapter = new PagerViewAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(pagerViewAdapter);
+
+        mViewPager.addOnPageChangeListener(pageChangeListener);
+
+        tv_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+        tv_allUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager.setCurrentItem(1);
+            }
+        });
+        tv_notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager.setCurrentItem(2);
+            }
+        });
+    }
+
+    ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+                changeTabs(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    //@TargetApi(Build.VERSION_CODES.M)
+    private void changeTabs(int position) {
+
+        if (position == 0){
+            tv_profile.setTextColor(ContextCompat.getColor(this,R.color.textTabBright));
+            tv_profile.setTextSize(22);
+
+            tv_allUsers.setTextSize(16);
+            tv_allUsers.setTextColor(ContextCompat.getColor(this,R.color.colorPrimary));
+
+            tv_notification.setTextSize(16);
+            tv_notification.setTextColor(ContextCompat.getColor(this,R.color.colorPrimary));
+        }
+        if (position == 1){
+            tv_profile.setTextColor(ContextCompat.getColor(this,R.color.colorPrimary));
+            tv_profile.setTextSize(16);
+
+            tv_allUsers.setTextSize(22);
+            tv_allUsers.setTextColor(ContextCompat.getColor(this,R.color.textTabBright));
+
+            tv_notification.setTextSize(16);
+            tv_notification.setTextColor(ContextCompat.getColor(this,R.color.colorPrimary));
+        }
+        if (position == 2){
+            tv_profile.setTextColor(ContextCompat.getColor(this,R.color.colorPrimary));
+            tv_profile.setTextSize(16);
+
+            tv_allUsers.setTextSize(16);
+            tv_allUsers.setTextColor(ContextCompat.getColor(this,R.color.colorPrimary));
+
+            tv_notification.setTextSize(22);
+            tv_notification.setTextColor(ContextCompat.getColor(this,R.color.textTabBright));
+        }
+
     }
 }
---
+++ app/src/main/java/com/job/showcase/userManagement/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.job.showcase.userManagement;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.job.showcase.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
